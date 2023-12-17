4803d7ad4accb31e21dcf6bf6926d7fc2e3b5444
Kamil-cr <136997351+Kamil-cr@users.noreply.github.com>
2023-12-17 19:18:58 +0500

    Cart Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,20 +16,25 @@ interface Props {
     color?: string
     category?: string
     size?: string
+    search?: string
   }
 }
 
 export default async function Page({searchParams}: Props) {
-  const {date, price, color, category, size} = searchParams
+  const {date, price, color, category, size, search} = searchParams
   const dateOrder = date ? `| order(_createdAt ${date})` : ``
   const priceOrder = price ? `| order(price ${price})` : ``
   const order = `${dateOrder}${priceOrder}`
 
-  const productFilter = `*[_type == "product"]`
-  const filter = ``
+  const productFilter = `_type == "product"`
+  const colorFilter = color ? `&& "${color}" in colors` : ``
+  const categoryFilter = category ? `&& "${category}" in categories` : ``
+  const sizeFilter = size ? `&& "${size}" in sizes` : ``
+  const searchFilter = search ? `&& name match "${search}"` : ``
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
 
   const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type == "product"] ${order}{
+    groq`${filter} ${order}{
     _id,
     _createdAt,
     name,
