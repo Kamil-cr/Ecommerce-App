1ea3cf3b9e3eb8e1b2db414c88c89823e5c1c833
Kamil-cr <136997351+Kamil-cr@users.noreply.github.com>
2023-12-18 01:00:56 +0500

    Stripe Integrated

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -5,4 +5,34 @@ import { validateCartItems } from "use-shopping-cart/utilities"
 import { inventory } from "@/config/inventory"
 import { stripe } from "@/lib/stripe"
 
-export async function POST() {}
+export async function POST(request: Request) {
+    const cartDetails = await request.json()
+    const lineItems = validateCartItems(inventory, cartDetails);
+    const origin = request.headers.get("origin")
+    try{
+    const session = await stripe.checkout.sessions.create({
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        line_items: lineItems,
+        shipping_address_collection: {
+            allowed_countries: ["US"],
+        },
+        shipping_options: [
+            {
+                shipping_rate: "shr_1OOP7TF4jaGjxyzWhHIJ1aSN"
+            }
+        ],
+        billing_address_collection: "auto",
+        success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${origin}/cart`,
+    });    
+    if (!session || typeof session !== 'object') {
+        throw new Error('Invalid session data');
+    }
+    return NextResponse.json(session)  
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Failed to create session' });
+    }
+}
--- src/components/cart-summary.tsx
+++ src/components/cart-summary.tsx
@@ -7,13 +7,25 @@ import { formatCurrencyString, useShoppingCart } from "use-shopping-cart"
 import { Button } from "@/components/ui/button"
 
 export function CartSummary() {
-  const { cartDetails, formattedTotalPrice, totalPrice, cartCount } = useShoppingCart()
+  const { cartDetails, formattedTotalPrice, totalPrice, cartCount, redirectToCheckout} = useShoppingCart()
   const [isLoading, setLoading] = useState(false)
   const isDisabled = isLoading || cartCount! === 0;
-  const shippingAmount = cartCount! > 0 ? 100 : 0; 
+  const shippingAmount = cartCount! > 0 ? 500 : 0; 
   const totalAmount = totalPrice! + shippingAmount;
 
-  function onCheckout() {}
+  async function onCheckout() {
+    setLoading(true)
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      body: JSON.stringify(cartDetails),
+    })
+    const data = await response.json();
+    const result = await redirectToCheckout(data.id)
+    if(result?.error) {
+      console.error(result)
+    }
+    setLoading(false)
+  }
 
   return (
     <section
